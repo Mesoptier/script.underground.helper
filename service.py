477955d68f08ca7b87d2162fd8a3cab8e48e8101
Koen Klaren <koen.klaren@gmail.com>
2016-02-08 20:27:29 +0100

    Add more properties

--- service.py
+++ service.py
@@ -8,10 +8,15 @@ def log(msg, level = 1):
     xbmc.log('Underground Helper Service :: ' + msg, level=xbmc.LOGNOTICE)
 
 class ContainerMonitor(threading.Thread):
-    timeout = 0.5
+    timeout = 0.05
     monitor = None
 
-    currentContent = ''
+    changed = True
+
+    previousContent = ''
+    content = ''
+    previousFolderPath = ''
+    folderPath = ''
 
     def __init__(self, *args):
         log('ContainerMonitor started')
@@ -23,11 +28,31 @@ class ContainerMonitor(threading.Thread):
             if (self.monitor.waitForAbort(self.timeout)):
                 break
 
+            if (self.changed == False):
+                self.changed = True
+                homeWindow.setProperty('Changed', 'True')
+
+            newFolderPath = xbmc.getInfoLabel('Container.FolderPath')
+            if (self.folderPath != newFolderPath):
+                self.previousFolderPath = self.folderPath
+                self.folderPath = newFolderPath
+                homeWindow.setProperty('PreviousFolderPath', self.previousFolderPath)
+                homeWindow.setProperty('FolderPath', self.folderPath)
+
+                self.changed = False
+                homeWindow.setProperty('Changed', 'False')
+
+                if (len(self.folderPath) < len(self.previousFolderPath)):
+                    homeWindow.setProperty('ChangedBackwards', 'True')
+                else:
+                    homeWindow.setProperty('ChangedBackwards', 'False')
+
             newContent = xbmc.getInfoLabel('Container.Content')
-            if (self.currentContent != newContent):
-                # log('content change ' + self.currentContent + ' to ' + newContent)
-                self.currentContent = newContent
-                homeWindow.setProperty('PreviousContent', newContent)
+            if (self.content != newContent):
+                self.previousContent = self.content
+                self.content = newContent
+                homeWindow.setProperty('PreviousContent', self.previousContent)
+                homeWindow.setProperty('Content', self.content)
 
 containerMonitor = ContainerMonitor()
 containerMonitor.start()
